bc681571ab6feb3e45952b56711fd1e7af0a5104
FongMi <b8892778@gmail.com>
2026-05-22 01:58:42 +0800

    Support application/octet-stream content

--- libraries/common/src/main/java/androidx/media3/common/MimeTypes.java
+++ libraries/common/src/main/java/androidx/media3/common/MimeTypes.java
@@ -136,6 +136,7 @@ public final class MimeTypes {
   public static final String APPLICATION_TX3G = BASE_TYPE_APPLICATION + "/x-quicktime-tx3g";
   public static final String APPLICATION_MP4VTT = BASE_TYPE_APPLICATION + "/x-mp4-vtt";
   public static final String APPLICATION_MP4CEA608 = BASE_TYPE_APPLICATION + "/x-mp4-cea-608";
+  public static final String APPLICATION_OCTET_STREAM = BASE_TYPE_APPLICATION + "/octet-stream";
 
   /**
    * @deprecated RawCC is a Google-internal subtitle format that isn't supported by this version of
